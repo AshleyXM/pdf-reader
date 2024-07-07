617c5b3bb3ee1ff6720d02db1bc5dd1e884c5c79
AshleyXM <454198339@qq.com>
2024-07-07 00:11:47 -0700

    Change the response content type of pdf link request

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Path, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, PlainTextResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from extract_workflow import extract_all_contents_from_pdf
@@ -18,8 +18,7 @@ async def root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})  # context must include a "request" key
 
 
-@app.get("/{pdf_url:path}")
+@app.get("/{pdf_url:path}", response_class=PlainTextResponse)
 async def parse_pdf(request: Request, pdf_url: str = Path(..., description="The URL of the PDF to process")):
-    print(pdf_url)
     parsed_pdf = extract_all_contents_from_pdf(pdf_url)
-    return templates.TemplateResponse("parsed_pdf.html", {"request": request, "parsed_pdf": parsed_pdf})
+    return parsed_pdf
