6b3e8310f0d1d73119b5c2046cbeafa39796aed2
AshleyXM <454198339@qq.com>
2024-07-08 13:49:11 -0700

    Manage constants in the project

---
+++ config/constants.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+# load env var
+load_dotenv()
+
+# read environment variables
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
+AWS_IMAGE_BUCKET = os.getenv('AWS_IMAGE_BUCKET')
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+IMG_TMP_SAVE_PATH = os.getenv('IMG_TMP_SAVE_PATH')
+PDF_TMP_SAVE_PATH = os.getenv('PDF_TMP_SAVE_PATH')
