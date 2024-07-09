cb72f0c20c1c7a0ee291f1aaceeff60aee919bc5
AshleyXM <454198339@qq.com>
2024-07-09 01:22:04 -0700

    Add two response types inherited from Response

--- schemas/Response.py
+++ schemas/Response.py
@@ -5,3 +5,13 @@ class Response(BaseModel):
     code: int
     data: str
     msg: str
+
+
+class OKResponse(Response):
+    def __init__(self, code=200, data="", msg="success"):
+        super().__init__(code=code, data=data, msg=msg)
+
+
+class ErrorResponse(Response):
+    def __init__(self, code=400, data="", msg="error"):
+        super().__init__(code=code, data=data, msg=msg)
