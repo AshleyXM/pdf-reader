120ea67caab3e35f88d51edb611ab24b94d99ca5
AshleyXM <454198339@qq.com>
2024-07-14 15:22:48 -0700

    Remove unused env variables

--- config/constants.py
+++ config/constants.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # read environment variables
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = os.getenv('AWS_REGION')
 AWS_IMAGE_BUCKET = os.getenv('AWS_IMAGE_BUCKET')
 
